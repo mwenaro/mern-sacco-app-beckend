e1c29ff8486ef2b6137be907717439c966b1cdbd
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-05-16 09:47:32 +0300

    Add initial files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      
+
+      
+    },
+  },
+  plugins: [
+  ]
+};
